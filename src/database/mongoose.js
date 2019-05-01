b824c86146df6e07f12015323f291acbcfb94694
Gabriel K <gabrielkukiel@gmail.com>
2019-05-01 02:02:03 -0400

    trying to solve heroku build issue

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 import "dotenv/config"
 
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true } )
+mongoose.connect(process.env.DATABASE_URL || "mongodb+srv://deploy:deploy@cluster0-ljd0z.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true } )
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true)
