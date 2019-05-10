f3c957f90341c9754cdc0b8da4087f1d055c2ed2
Gabriel K <gabrielkukiel@gmail.com>
2019-05-10 14:18:45 -0400

    Changing the RGB generation method. Thx to danielstockton!

--- src/models/turma.js
+++ src/models/turma.js
@@ -6,6 +6,6 @@ const Schema = mongoose.Schema;
 const localModel = new Schema({
     id: { type: String, default:cuid()   },
     nome: { type: String, required: true},
-    color: {type: String, default:(() => '#'+(Math.random()*0xFFFFFF<<0).toString(16))}
+    color: {type: String, default:(() => "#000000".replace(/0/g,()=>(~~(Math.random()*16)).toString(16)))}
 })
 export default mongoose.model('Turma', localModel)
