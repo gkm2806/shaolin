d34b88e1d6efd8533ee68715758e454520c22ddf
gkm <gabrielkukiel@gmail.com>
2020-06-05 22:37:44 -0300

    [hotfix] mongoose connection

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -7,7 +7,6 @@ mongoose.connect("mongodb+srv://deploy:deploy@cluster0-ljd0z.mongodb.net/test?re
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true)
-mongoose.set('find', false)
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
